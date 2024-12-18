7388fe55af69c2ba5d23d6a8ee25041079d17597
Junhyeok Song <167317714+peakearth@users.noreply.github.com>
2024-12-18 13:24:21 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 108 ms, Memory: 112308 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+current_time = datetime.utcnow()
+
+year = current_time.year
+month = current_time.month
+day = current_time.day
+
+print(year)
+print(month)
+print(day)
